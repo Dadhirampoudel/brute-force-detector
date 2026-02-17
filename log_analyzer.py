e2bc4a166278a5b05610d97e101470973b4b36dd
Dadhirampoudel <154711617+Dadhirampoudel@users.noreply.github.com>
2026-02-18 00:10:58 +1100

    Fix syntax error in log_analyzer.py

--- log_analyzer.py
+++ log_analyzer.py
@@ -7,7 +7,7 @@ def detect_brute_force(log_file, threshold=3):
       ip = parts[0]
       status = parts[1]
 
-if status == "FAILED"
+if status == "FAILED":
   if ip in failed_attempts:
     failed_attempts[ip] += 1
   else:
