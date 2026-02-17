6a7b88331ad27608d2ca72744d14ccc5cea8625d
Dadhirampoudel <154711617+Dadhirampoudel@users.noreply.github.com>
2026-02-17 23:33:54 +1100

    Add brute force detection function
    Implement brute force detection from log file.

---
+++ log_analyzer.py
@@ -0,0 +1,22 @@
+def detect_brute_force(log_file, threshold=3):
+  failed_attempts = {}
+
+  with open(log_file, "r") as file:
+    for line in fil:
+      parts = line.strip().split(" - ")
+      ip = parts[0]
+      status = parts[1]
+
+if status == "Failed":
+  if ip in failed_attempts:
+    failed_attempts[ip] += 1
+  else:
+    failed_attempts[ip] = 1
+
+print("Suspicious IPs (Possible Brute Force Attacks):")
+for ip, count in failed_attempts.items():
+  if count >= threshold:
+    print(f"{ip} -> {count} failed attempts")
+
+if __name__ == " __main__":
+  detect_brute_force("sample_log.txt")
